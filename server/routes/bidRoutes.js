30a167df8624626a204c06bd0c8b45ce290d28c0
nitinn0 <nitinkapoor146@gmail.com>
2025-06-21 20:03:59 +0530

    Remove all files from repository

--- server/routes/bidRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const bidController = require("../controllers/bidControllers");
-const gemini = require("../utils/gemini");
-
-router.post('/memes/:memeId/bid', bidController.placeBid);
-
-module.exports = router;
